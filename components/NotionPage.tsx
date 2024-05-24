9c8248e996a944e72cb096a60333e0a24739f1dc
devMuromi <devmuromi@gmail.com>
2024-05-24 22:41:56 +0900

    remove: github corner

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -25,7 +25,6 @@ import { searchNotion } from '@/lib/search-notion'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import { Footer } from './Footer'
-import { GitHubShareButton } from './GitHubShareButton'
 import { Loading } from './Loading'
 import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
@@ -309,8 +308,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
         pageAside={pageAside}
         footer={footer}
       />
-
-      <GitHubShareButton />
     </>
   )
 }
